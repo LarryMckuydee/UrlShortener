e27671a6b96c850ee51e4d820eafadec4d9f1dcb
LarryMckuydee <larry.mckuydee@gmail.com>
2015-07-18 17:37:40 +0800

    Add Style

--- home.php
+++ home.php
@@ -14,20 +14,22 @@
 ?>
 <html>
 	<head>
-
+		<link rel="stylesheet" type="text/css" href="css/home.css">
 	</head>
 	<body>
-		<h2> Test Generate Shortener URL</h2>
+		<h2> Shorten Your Link Here</h2>
 		<?php
 			if(isset($_SESSION['msg'])){
 				echo "<div>".$_SESSION['msg']."</div>";
 				unset($_SESSION['msg']);
 
+			}else{
+				echo "<br/>";
 			}
 		?>
 		<form method="post" action="generate.php">
 			<input type="text" name="url">
-			<input type="submit" >
+			<input type="submit" value="Generate">
 		</form>
 	</body>
 </html>
